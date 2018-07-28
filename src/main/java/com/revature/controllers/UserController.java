7000bf2d4f9fc03911b0a81f7f1f4c3a2e804422
Bradley-Walker <bradleynwalker1@gmail.com>
2018-07-28 16:05:47 -0400

    Added the @CrossOrigin annotation to the UserController

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.beans.User;
 import com.revature.services.UserService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value="/users")
 public class UserController {
